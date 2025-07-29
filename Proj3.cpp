686d95c332928d22d2802f58845cd7878450c3e7
Zohaib Ashraf <zohaibashraf2559@gmail.com>
2025-07-29 14:34:21 +0500

    Add files via upload

---
+++ Proj3.cpp
@@ -0,0 +1,21 @@
+// Tell the user a is greater or b
+
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+    int a, b;
+    cout << "Enter two numbers:";
+    cin >> a >> b;
+    
+    if ( a>b )
+    {
+        cout << "A is greater than B."<<endl;
+    }
+    else 
+    {
+        cout << "B is greater than A."<<endl;
+    }
+    return 0;
+}
